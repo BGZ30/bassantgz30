f007c74d837ee6e7bddda6e783af594a5b09da7e
bgz30 <bassant.gamalzaid@gmail.com>
2021-01-01 22:28:52 +0200

    removed url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: '',
   pathPrefix: '/',
   title: 'BassantGz30',
   subtitle: 'Software Engineer. Enthusiast about Data Science. I Write about Machine Learning and some other topics',,
